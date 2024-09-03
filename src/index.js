b0c53f22994b5ca9d7f863d74ae4d479e22a9797
Paolo Caleffi <p.caleffi@dreamonkey.com>
2024-09-03 15:44:41 +0200

    fix(vitest): align compatibility range for quasar package

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = function (api) {
-  api.compatibleWith('quasar', '^2.6.0');
+  api.compatibleWith('quasar', '^2.12.7');
   // Vitest 1.0 requires Vite 5, which is only officially supported by @quasar/app-vite v2
   // Yet, there are workarounds to make @quasar/app-vite@v1 work with Vite 5, so we aren't restricting this AE only to v2
   // See https://github.com/quasarframework/quasar/issues/14077
